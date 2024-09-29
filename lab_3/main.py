f816e72dbf431ae4a7ab4d29027a4ba2ba1bcd9c
“Anisimova <anisimova.marja@yandex.ru>
2024-09-29 21:12:15 +0400

    Finished lab3

--- lab_3/main.py
+++ lab_3/main.py
@@ -24,16 +24,33 @@ def menu():
                 ))
             print(f"Your key length: {key_length} ")
 
-            symmetric.generation_key(key_length)
+            assym_keys = assymetric.generate_keys()
+            assymetric.serialization_private(assym_keys["private_key"], settings["secret_key_path"])
+            assymetric.serialization_public(assym_keys["public_key"], settings["public_key_path"])
+            key_serialization= symmetric.generation_key(key_length)
+            symmetric.key_serialization(key_serialization, settings["symmetric_key_path"])
 
         case args if args.encryption:
-            pass
+            symmetric_key = symmetric.key_deserialization(settings["symmetric_key_path"])
+            symmetric.encrypt(symmetric_key, settings["initial_file_path"], settings["encrypted_file_path"])
+
         case args if args.decryption:
-            pass
+            symmetric_key = symmetric.key_deserialization(settings["symmetric_key_path"])
+            symmetric.decrypt(symmetric_key, settings["encrypted_file_path"], settings["decrypted_file_path"])
+
         case args if args.encryption_symmetric:
-            pass
+            symmetric_key = symmetric.key_deserialization(settings["symmetric_key_path"])
+            public_key = assymetric.public_key_deserialization(settings["public_key_path"])
+
+            encrypted_symmetric_key = assymetric.encrypt(public_key, symmetric_key)
+            write_binary_to_file(settings["encrypted_key_path"], encrypted_symmetric_key)
+
         case args if args.decryption_symmetric:
-            pass
+            private_key = assymetric.private_key_deserialization(settings["secret_key_path"])
+            encrypted_symmetric_key = read_binary_from_file(settings["encrypted_key_path"])
+
+            decrypted_symmetric_key = assymetric.decrypt(private_key, encrypted_symmetric_key)
+            write_binary_to_file(settings["decrypted_key_path"], decrypted_symmetric_key)
 
 
 if __name__ == "__main__":
