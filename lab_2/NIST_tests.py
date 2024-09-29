0ac7e908246ab6f1374972996890f8ebf43316c1
“Anisimova <anisimova.marja@yandex.ru>
2024-09-29 17:35:38 +0400

    Add nist_identical_serial_bits

--- lab_2/NIST_tests.py
+++ lab_2/NIST_tests.py
@@ -19,4 +19,22 @@ def nist_frequency_bit_test(bit_sequence: str, file_path: str, key: str) -> None
         print("Frequency bit test. Error: ", e)
 
 
+def nist_identical_serial_bits(bit_sequence: str, file_path: str, key: str) -> None:
 
+    try:
+        n = len(bit_sequence)
+        ones_count = bit_sequence.count("1")
+        share_of_unit = ones_count / n
+        if abs(share_of_unit - 0.5) < (2 / math.sqrt(n)):
+            v = 0
+            for bit in range(n - 1):
+                if bit_sequence[bit] != bit_sequence[bit + 1]:
+                    v += 1
+            numerator = abs(v - 2 * n * share_of_unit * (1 - share_of_unit))
+            denominator = 2 * math.sqrt(2 * n) * share_of_unit * (1 - share_of_unit)
+            p_v = math.exp(numerator / denominator)
+            write_text_to_file(file_path, f'{key} : {p_v}\n')
+    except ZeroDivisionError:
+        print("Identical serial bits test. Error: Division by zero")
+    except Exception as e:
+        print("Identical serial bits test. Error: ", e)
--- lab_2/main.py
+++ lab_2/main.py
@@ -3,10 +3,15 @@ from work_with_files import *
 
 if __name__ == "__main__":
    settings = read_json_from_file("settings.json")
+
    sequences = read_json_from_file(settings["generated_sequences"])
    sequences_cpp = sequences["cpp_generator"]
    sequences_java = sequences["java_generator"]
+
    nist_frequency_bit_test(sequences_cpp, settings["result_of_tests"], "C++")
    nist_frequency_bit_test(sequences_java, settings["result_of_tests"], "Java")
- 
+   
+   nist_identical_serial_bits(sequences_cpp, settings["result_of_tests"], "C++")
+   nist_identical_serial_bits(sequences_java, settings["result_of_tests"], "Java")
+
    print('Successful work!')
