cc3af4b1eb94d29ebf761f9d9c1eb6eadc84a586
“Anisimova <anisimova.marja@yandex.ru>
2024-09-29 19:39:14 +0400

    Make main menu

---
+++ lab_3/main.py
@@ -0,0 +1,33 @@
+import argparse
+from work_with_files import *
+
+
+def menu():
+
+    parser = argparse.ArgumentParser()
+    settings = read_json_from_file("settings.json")
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument('-gen', '--generation', help='Generate symmetric and asymmetric keys')
+    group.add_argument('-enc', '--encryption', help='Encrypt a file using symmetric key')
+    group.add_argument('-dec', '--decryption', help='Decrypt a file using symmetric key')
+    group.add_argument('-enc_sym', '--encryption_symmetric', help='Encrypt sym key using asym encryption')
+    group.add_argument('-dec_sym', '--decryption_symmetric', help='Decrypt sym key using asym decryption')
+
+
+    args = parser.parse_args()
+
+    match args:
+        case args if args.generation:
+            pass
+        case args if args.encryption:
+            pass
+        case args if args.decryption:
+            pass
+        case args if args.encryption_symmetric:
+            pass
+        case args if args.decryption_symmetric:
+            pass
+
+
+if __name__ == "__main__":
+    menu()
--- lab_3/work_with_files.py
+++ lab_3/work_with_files.py
@@ -44,3 +44,25 @@ def write_json_to_file(file_path: str, data: dict, indent: int = 4) -> None:
         print("Data was successfully written to a file in JSON.")
     except Exception as e:
         print(f"An error occurred while writing to the file: {e}")
+
+
+def read_binary_from_file(file_path: str) -> bytes:
+    try:
+        with open(file_path, 'rb') as file:
+            data = file.read()
+        return data
+    except FileNotFoundError as e:
+        print(f"\033[97;41mFile not found: {e}\033[0m")
+    except Exception as e:
+        print(f"\033[97;41mAn error occurred while reading the file: {e}\033[0m")
+
+
+def write_binary_to_file(file_path: str, bytes_text: bytes) -> None:
+    try:
+        with open(file_path, 'wb') as file:
+            file.write(bytes_text)
+        print("\033[91;107mThe binary data has been successfully saved to the file.\033[0m")
+    except FileNotFoundError as e:
+        print(f"\033[97;41mFile not found: {e}\033[0m")
+    except Exception as e:
+        print(f"\033[97;41mAn error occurred while writing the binary data to the file: {e}\033[0m")
